060e7503b6368f103520ae2b7d2aa8ef2e1311fb
Eliana Specogna <specognaeliana@gmail.com>
2025-04-29 22:28:55 -0300

    Iniciando proyecto en spring boot

---
+++ gestionCuentas/src/main/java/com/banco/gestionCuentas/GestionCuentasApplication.java
@@ -0,0 +1,13 @@
+package com.banco.gestionCuentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCuentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCuentasApplication.class, args);
+	}
+
+}
---
+++ gestionCuentas/src/test/java/com/banco/gestionCuentas/GestionCuentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.gestionCuentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCuentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
